31d5266266b81667ee713a5ce73945dc91829d85
Raj <raj4445567@gmail.com>
2026-04-01 17:46:54 +0530

    Following Links in HTML Using BeautifulSoup

---
+++ href-soup.py
@@ -0,0 +1,25 @@
+# http://py4e-data.dr-chuck.net/known_by_Fikret.html (Sample Problem)
+# http://py4e-data.dr-chuck.net/known_by_Denise.html (Actual Problem)
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter Url: ')
+count = int(input('Enter count: '))
+posi = int(input('Enter position: '))
+
+for i in range(count):   
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tags = soup('a')
+    
+    tag = tags[posi - 1]
+    url = tag.get('href', None)
+    
+    print("Retrieving:", url)
